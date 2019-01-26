536c360943a11039f6cbb92b8251a750b9cd7934
SponsoredO <onasisaboagyeowusu99@gmail.com>
2019-01-26 12:06:40 +0000

    javascript modifies developer field

---
+++ main.js
@@ -0,0 +1,8 @@
+window.onload = function (e){
+
+    alert(" Today is the first day of the rest of your life ! ");
+
+    var barnabas = document.getElementById('developer');
+    console.log(barnabas.innerText);
+    barnabas.innerText =' onaaapo dey be ';
+}; 
